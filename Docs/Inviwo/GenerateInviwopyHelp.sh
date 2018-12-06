46a57a0231f4516a04f6d6a020bb290e00b90d91
sean <martins7@tcd.ie>
2018-12-06 16:29:14 +0000

    Updated help scripts for Inviwo

--- Docs/Inviwo/GenerateInviwopyHelp.sh
+++ Docs/Inviwo/GenerateInviwopyHelp.sh
@@ -4,5 +4,3 @@
 # NOTE the document generator code from inviwo can be faulty and need to be modified
 # Close inviwo after generating documents
 ~/inviwo_build/bin/inviwo -w ~/inviwo/data/workspaces/boron.inv -p ~/inviwo/modules/python3/scripts/documentgenerator.py
-
-google-chrome ~/inviwo/data/help/inviwopy.html
---
+++ Docs/Inviwo/ShowInviwopyHelp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+google-chrome ~/inviwo/data/help/inviwopy.html
