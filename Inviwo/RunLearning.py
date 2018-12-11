19eabbcac3135db355ab2b31f1f98389542b0c82
sean <martins7@tcd.ie>
2018-12-11 11:31:53 +0000

    Code does not quite work due to communication with gym. Could try to take into Unity instead

---
+++ Inviwo/RunLearning.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.insert(0, "/home/sean/py_environments/openai_baslines/lib/python3.5/site-packages")
+
+sys.path.insert(0, 
+"/home/sean/TransferFunctionLearning/Inviwo")
+
+import gym
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines import PPO2
+
+from inviwo_env import InviwoEnv
+
+env = InviwoEnv()
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
